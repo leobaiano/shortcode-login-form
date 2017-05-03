6c1331093e53ba1b31e3cb0dbdf298afe41edce6
marioernestoms <marioernesto.ms@gmail.com>
2017-05-02 22:01:26 -0300

    include current user name and avatar,and button to logout

--- shortcode-login-form.php
+++ shortcode-login-form.php
@@ -1,30 +1,31 @@
 <?php
-	/**
-	 * Plugin Name: Shortcode Login Form
-	 * Plugin URI:
-	 * Description: Create shortcode for add login form in pages
-	 * Author: leobaiano
-	 * Author URI: http://leobaiano.com.br
-	 * Version: 1.0.0
-	 * License: GPLv2 or later
-	 * Text Domain: lb-slf
- 	 * Domain Path: /languages/
-	 */
+/**
+ * Plugin Name: Shortcode Login Form
+ * Plugin URI:
+ * Description: Create shortcode for add login form in pages
+ * Author: leobaiano
+ * Author URI: http://leobaiano.com.br
+ * Version: 1.0.0
+ * License: GPLv2 or later
+ * Text Domain: lb-slf
+ * Domain Path: /languages/
+ */
+function lb_shortcode_login_form( $atts ) {
+	global $current_user;
+	wp_get_current_user();
+	$defaults = array(
+		'message_logged'	=> __( 'Logged in!', 'lb-slf' ),
+		'label_username'	=> __( 'Username', 'lb-slf' ),
+		'label_password'	=> __( 'Password', 'lb-slf' ),
+		'label_remember'	=> __( 'Remember Me', 'lb-slf' ),
+		'label_log_in'		=> __( 'Log In', 'lb-slf' ),
+	);
+	$atts = shortcode_atts( $defaults, $atts, 'lb-login-form' );
+	$atts['echo'] = true;
 
-	function lb_shortcode_login_form( $atts ) {
-		$defaults = array(
-			'message_logged'	=> __( 'Logged in!', 'lb-slf' ),
-			'label_username'	=> __( 'Username', 'lb-slf' ),
-			'label_password'	=> __( 'Password', 'lb-slf' ),
-			'label_remember'	=> __( 'Remember Me', 'lb-slf' ),
-			'label_log_in'		=> __( 'Log In', 'lb-slf' ),
-		);
-		$atts = shortcode_atts( $defaults, $atts, 'lb-login-form' );
-		$atts['echo'] = false;
-
-		if ( is_user_logged_in() )
-			return '<p>' . $atts['message_logged'] . '</p>';
-
-		return wp_login_form( $atts );
-	}
-	add_shortcode( 'lb-login-form', 'lb_shortcode_login_form' );
+	if ( is_user_logged_in() )
+	return '<p>' . get_avatar( $current_user->ID, 26 ) . ' <span>' . $current_user->display_name . '</span> <a href="' . wp_logout_url(get_permalink()) . '">Logout</a></p>';
+	else
+	return wp_login_form( $atts );
+}
+add_shortcode( 'lb-login-form', 'lb_shortcode_login_form' );
