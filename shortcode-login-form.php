e14c54d3555c76f2a9b891034b1a0d9ae20d6ed0
Leo Baiano <ljunior2005@gmail.com>
2017-05-02 23:10:29 -0300

    Removendo identamento errado

--- shortcode-login-form.php
+++ shortcode-login-form.php
@@ -1,30 +1,30 @@
 <?php
-	/**
-	 * Plugin Name: Shortcode Login Form
-	 * Plugin URI:
-	 * Description: Create shortcode for add login form in pages
-	 * Author: leobaiano
-	 * Author URI: http://leobaiano.com.br
-	 * Version: 1.0.0
-	 * License: GPLv2 or later
-	 * Text Domain: lb-slf
- 	 * Domain Path: /languages/
-	 */
+/**
+ * Plugin Name: Shortcode Login Form
+ * Plugin URI:
+ * Description: Create shortcode for add login form in pages
+ * Author: leobaiano
+ * Author URI: http://leobaiano.com.br
+ * Version: 1.0.0
+ * License: GPLv2 or later
+ * Text Domain: lb-slf
+	 * Domain Path: /languages/
+ */
 
-	function lb_shortcode_login_form( $atts ) {
-		$defaults = array(
-			'message_logged'	=> __( 'Logged in!', 'lb-slf' ),
-			'label_username'	=> __( 'Username', 'lb-slf' ),
-			'label_password'	=> __( 'Password', 'lb-slf' ),
-			'label_remember'	=> __( 'Remember Me', 'lb-slf' ),
-			'label_log_in'		=> __( 'Log In', 'lb-slf' ),
-		);
-		$atts = shortcode_atts( $defaults, $atts, 'lb-login-form' );
-		$atts['echo'] = false;
+function lb_shortcode_login_form( $atts ) {
+	$defaults = array(
+		'message_logged'	=> __( 'Logged in!', 'lb-slf' ),
+		'label_username'	=> __( 'Username', 'lb-slf' ),
+		'label_password'	=> __( 'Password', 'lb-slf' ),
+		'label_remember'	=> __( 'Remember Me', 'lb-slf' ),
+		'label_log_in'		=> __( 'Log In', 'lb-slf' ),
+	);
+	$atts = shortcode_atts( $defaults, $atts, 'lb-login-form' );
+	$atts['echo'] = false;
 
-		if ( is_user_logged_in() )
-			return '<p>' . $atts['message_logged'] . '</p>';
+	if ( is_user_logged_in() )
+		return '<p>' . $atts['message_logged'] . '</p>';
 
-		return wp_login_form( $atts );
-	}
-	add_shortcode( 'lb-login-form', 'lb_shortcode_login_form' );
+	return wp_login_form( $atts );
+}
+add_shortcode( 'lb-login-form', 'lb_shortcode_login_form' );
